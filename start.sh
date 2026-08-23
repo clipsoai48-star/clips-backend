984b7bf7c1626525abccc7e147d6080d85325f0b
Lanzo <clipsoai48@gmail.com>
2026-08-23 15:59:12 +0100

    Fix start script

--- start.sh
+++ start.sh
@@ -1,1 +1,3 @@
-#!/bin/bash rq worker --worker-class rq.worker.SimpleWorker clipso_jobs_priority clipso_jobs & uvicorn main:app --host 0.0.0.0 --port $PORT
+#!/bin/bash
+rq worker --worker-class rq.worker.SimpleWorker clipso_jobs_priority clipso_jobs &
+uvicorn main:app --host 0.0.0.0 --port $PORT
